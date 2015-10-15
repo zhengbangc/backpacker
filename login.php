fd340e6119c2f305477a59c883805a05a2e68a59
Yifei Li <liyifei.leo@gmail.com>
2015-10-15 15:36:46 -0500

    login done

--- login.php
+++ login.php
@@ -1,3 +1,11 @@
+<?php
+include('checklogin.php'); // Includes Login Script
+
+//if(isset($_SESSION['login_user'])){
+//header("location: index.php");
+//}
+?>
+
 <html>
 <head>
 <title>Log in</title>
@@ -6,13 +14,14 @@
 </head>
 
 <body>
-<form action="">
+<form action="" method="post" autocomplete="off">
   <h2><span class="entypo-login"></span> Login</h2>
-  <button class="submit"><span class="entypo-lock"></span></button>
+  <button name="submit" class="submit"><span class="entypo-lock"></span></button>
   <span class="entypo-user inputUserIcon"></span>
-  <input type="text" class="user" placeholder="ursername"/>
+  <input name="username" type="text" class="user" placeholder="ursername"/>
   <span class="entypo-key inputPassIcon"></span>
-  <input type="password" class="pass"placeholder="password"/>
+  <input name="password" type="password" class="pass"placeholder="password" />
+  <span>  <?php echo $error; ?></span>
 </form>
 </body>
 </html>
