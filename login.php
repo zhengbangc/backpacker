a91931051e8cd670da6f204afd4e26693f795b60
Yifei Li <liyifei.leo@gmail.com>
2015-10-15 01:27:27 -0500

    -finish search

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Log in</title>
+<link href="login.css" rel="stylesheet">
+<script type="text/javascript" src="login.js"></script>
+</head>
+
+<body>
+<form action="">
+  <h2><span class="entypo-login"></span> Login</h2>
+  <button class="submit"><span class="entypo-lock"></span></button>
+  <span class="entypo-user inputUserIcon"></span>
+  <input type="text" class="user" placeholder="ursername"/>
+  <span class="entypo-key inputPassIcon"></span>
+  <input type="password" class="pass"placeholder="password"/>
+</form>
+</body>
+</html>
